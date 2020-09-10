2016d494ec86ace02f1ca05371fe586cdd73d28d
Auganbay <auganenu@gmail.com>
2020-09-10 17:44:35 +0600

    added Url model

--- urls_app/admin.py
+++ urls_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+    )
+
--- urls_app/models.py
+++ urls_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    text = models.CharField(
+        max_length=2000,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+
